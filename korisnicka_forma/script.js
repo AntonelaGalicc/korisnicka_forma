db5bdff95a188c34d5683245040e99e53ff71bd9
Antonela Galić <antonela.galic@fpmoz.sum.ba>
2024-07-05 09:39:13 +0200

    Fali kod

---
+++ korisnicka_forma/script.js
@@ -0,0 +1,10 @@
+let inputs = document.querySelector("input");
+let errors = {
+    "ime_prezime" : [],
+    "korisnicko_ime" : [],
+    "email" : [],
+    "lozinka" : [],
+    "ponovi_lozinku" : []
+};
+
+console.log(errors);
